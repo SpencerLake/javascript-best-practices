c38457a7ea8f3df82b0ff49290070ee826ce5ac3
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-08-24 09:36:06 -0600

    javascript-best-practices

---
+++ js.best-practices.js
@@ -0,0 +1,13 @@
+//Best Practices
+// 1. Use '===' instead of '=='
+// 2. Stick to a strick coding style
+// 3. Declarations on top of each script or function
+// 4. Declare Objects and Arrays with 'const'
+// 5. Embrace for progressive enhancive
+// 6. Initialize Variables (Have all variables at top of code)
+// 7. Beware of Automatic Type Conversions
+// 8. Don't be lazy when naming variables
+// 9. Avoid Numbers, String, and Boolean as Objects
+// 10. Avoid Using eval()
+// 11. Use SemiColons
+
